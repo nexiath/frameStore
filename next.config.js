234803e479be66cd0a1916d312f689ee664444cc
nexiath <robin.cassard39@gmail.com>
2025-06-28 18:25:59 +0200

    modif

--- next.config.js
+++ next.config.js
@@ -4,6 +4,25 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  transpilePackages: ['@coinbase/wallet-sdk'],
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    
+    config.externals.push({
+      'utf-8-validate': 'commonjs utf-8-validate',
+      'bufferutil': 'commonjs bufferutil',
+    });
+    
+    return config;
+  },
   async headers() {
     return [
       {
